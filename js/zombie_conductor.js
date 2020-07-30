9f6a50cf70699656566edb4aab3e28b9b6f5bde9
Marcio <55676026+marcio-git@users.noreply.github.com>
2020-07-29 22:07:07 -0300

    Adding files to the repository

---
+++ js/zombie_conductor.js
@@ -0,0 +1,32 @@
+/* Para insipirarte para la implementacion del ZombieConductor podes usar
+al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
+Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
+no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
+del ZombieCaminante que eran los mismos. */
+
+var ZombieConductor = function (sprite, x, y, ancho, alto, velocidad, rangoMov, direccion) {
+  /* Completar constructor a partir de Enemigo */
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
+  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  this.direccion = direccion
+}
+
+/* Completar creacion del ZombieConductor */
+ZombieConductor.prototype = Object.create(Enemigo.prototype);
+ZombieConductor.prototype.constructor = ZombieConductor;
+/* Completar metodos para el movimiento y el ataque */
+
+ZombieConductor.prototype.mover = function () {
+  if (this.direccion == 'v') {
+    this.y += this.velocidad;
+    if (this.y > this.rangoMov.hastaY) {
+      this.y = -this.alto
+    }
+  }
+  if (this.direccion == 'h') {
+    this.x += this.velocidad;
+    if (this.x > this.rangoMov.hastaX) {
+      this.x = -this.ancho
+    }
+  }
+}
